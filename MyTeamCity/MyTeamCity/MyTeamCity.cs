bda6d6580af06a6cb82dd8bb338e7abbf9e12aa9
prinay.panday <prinay.panday@entelect.co.za>
2015-11-25 15:04:42 +0200

    Added more functionality

--- MyTeamCity/MyTeamCity/MyTeamCity.cs
+++ MyTeamCity/MyTeamCity/MyTeamCity.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -137,5 +138,143 @@ namespace MyTeamCity
                 return JsonConvert.DeserializeObject<TeamCityBuildList>(responseResult);
             }
         }
+
+        public async Task<TeamCityBuildList> GetBuildsSinceDate(DateTime date)
+        {
+            using (webClient = new HttpClient())
+            {
+                webClient.BaseAddress = new Uri(TeamCityServer);
+                webClient.DefaultRequestHeaders.Accept.Clear();
+                webClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                
+                var encoded = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1")
+                  .GetBytes(
+                  Username + ":" + Password));
+                webClient.DefaultRequestHeaders.Add("Authorization", "Basic " + encoded);
+                
+                var response = await webClient.GetAsync("app/rest/builds?locator=sinceDate:" +
+                    date.ToString("yyyyMMdd'T'HHmmsszzzz")
+                    .Replace(":", "")
+                    .Replace("+", "-"));
+                var responseAsyncString = response.Content.ReadAsStringAsync();
+                var responseResult = responseAsyncString.Result;
+
+                return JsonConvert.DeserializeObject<TeamCityBuildList>(responseResult);
+            }
+        }
+
+        public async Task<TeamCityBuildList> GetAllBuildsWithStatusSinceDate(DateTime date, BuildStatus buildStatus)
+        {
+            using (webClient = new HttpClient())
+            {
+                webClient.BaseAddress = new Uri(TeamCityServer);
+                webClient.DefaultRequestHeaders.Accept.Clear();
+                webClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var encoded = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1")
+                  .GetBytes(
+                  Username + ":" + Password));
+                webClient.DefaultRequestHeaders.Add("Authorization", "Basic " + encoded);
+
+                var response = await webClient.GetAsync("app/rest/builds?locator=sinceDate:" +
+                    date.ToString("yyyyMMdd'T'HHmmsszzzz")
+                    .Replace(":", "")
+                    .Replace("+", "-") +"," 
+                    +"status:" + Enum.GetName(typeof(BuildStatus), buildStatus));
+                var responseAsyncString = response.Content.ReadAsStringAsync();
+                var responseResult = responseAsyncString.Result;
+
+                return JsonConvert.DeserializeObject<TeamCityBuildList>(responseResult);
+            }
+        }
+
+
+        public async Task<TeamCityBuildList> GetAllBuildsWithStatus(BuildStatus buildStatus)
+        {
+            using (webClient = new HttpClient())
+            {
+                webClient.BaseAddress = new Uri(TeamCityServer);
+                webClient.DefaultRequestHeaders.Accept.Clear();
+                webClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var encoded = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1")
+                  .GetBytes(
+                  Username + ":" + Password));
+                webClient.DefaultRequestHeaders.Add("Authorization", "Basic " + encoded);
+
+                var response = await webClient.GetAsync("app/rest/builds?locator=status:" + buildStatus);
+                var responseAsyncString = response.Content.ReadAsStringAsync();
+                var responseResult = responseAsyncString.Result;
+
+                return JsonConvert.DeserializeObject<TeamCityBuildList>(responseResult);
+            }
+        }
+
+        public async Task<TeamCityBuildList> GetBuildsByUser(string username)
+        {
+            using (webClient = new HttpClient())
+            {
+                webClient.BaseAddress = new Uri(TeamCityServer);
+                webClient.DefaultRequestHeaders.Accept.Clear();
+                webClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var encoded = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1")
+                  .GetBytes(
+                  Username + ":" + Password));
+                webClient.DefaultRequestHeaders.Add("Authorization", "Basic " + encoded);
+
+                var response = await webClient.GetAsync("app/rest/builds?locator=user:" + username);
+                var responseAsyncString = response.Content.ReadAsStringAsync();
+                var responseResult = responseAsyncString.Result;
+
+                return JsonConvert.DeserializeObject<TeamCityBuildList>(responseResult);
+            }
+        }
+
+        public async Task<TeamCityBuildList> GetBuildsByUserWithStatus(string username, BuildStatus buildStatus)
+        {
+            using (webClient = new HttpClient())
+            {
+                webClient.BaseAddress = new Uri(TeamCityServer);
+                webClient.DefaultRequestHeaders.Accept.Clear();
+                webClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var encoded = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1")
+                  .GetBytes(
+                  Username + ":" + Password));
+                webClient.DefaultRequestHeaders.Add("Authorization", "Basic " + encoded);
+
+                var response = await webClient.GetAsync("app/rest/builds?locator=user:" + username + "," + "status:" + buildStatus);
+                var responseAsyncString = response.Content.ReadAsStringAsync();
+                var responseResult = responseAsyncString.Result;
+
+                return JsonConvert.DeserializeObject<TeamCityBuildList>(responseResult);
+            }
+        }
+
+        public async Task<BaseBuild> GetLastBuildWithStatusForProject(BuildStatus buildStatus, string projectName)
+        {
+            using (webClient = new HttpClient())
+            {
+                webClient.BaseAddress = new Uri(TeamCityServer);
+                webClient.DefaultRequestHeaders.Accept.Clear();
+                webClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var encoded = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1")
+                  .GetBytes(
+                  Username + ":" + Password));
+                webClient.DefaultRequestHeaders.Add("Authorization", "Basic " + encoded);
+
+                var response = await webClient.GetAsync("app/rest/builds?locator=status:" + buildStatus + "," + "project:" + projectName);
+                var responseAsyncString = response.Content.ReadAsStringAsync();
+                var responseResult = responseAsyncString.Result;
+
+                var builds = JsonConvert.DeserializeObject<TeamCityBuildList>(responseResult);
+                return builds.build.OrderByDescending(x => x.id).FirstOrDefault();
+            }
+        }
+        
+
+
     }
 }
