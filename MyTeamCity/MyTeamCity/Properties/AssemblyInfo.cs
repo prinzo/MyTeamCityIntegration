e5089f8d18c9c66612c38427d2b2ab0c6a4f27b4
prinay.panday <prinay.panday@entelect.co.za>
2015-11-25 08:00:27 +0200

    Restart

--- MyTeamCity/MyTeamCity/Properties/AssemblyInfo.cs
+++ MyTeamCity/MyTeamCity/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("MyTeamCity")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("MyTeamCity")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("dab23578-6bb3-4204-bf1d-e621394357c0")]
+[assembly: Guid("2ffb5793-bc85-403f-822e-96935da6212c")]
 
 // Version information for an assembly consists of the following four values:
 //
--- README.md
+++
@@ -1,8 +0,0 @@
-# MyTeamCityIntegration
-TeamCity Integration Using C#
-
-The following application communicates with TeamCity's API
-
-I am currently working on a nuget package for .Net applications to communicate with TeamCity
-
-Currently the app is small and only retrieves all builds from TeamCity
